48b93c9948470d512f4de81714ad7fc9dac4187c
dundee-zhang <dundeez@gmx.com>
2021-07-05 20:33:49 -0400

    cmake

---
+++ cmake-build-debug/CMakeFiles/Better_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Better_Calculator"
+  "Better_Calculator.pdb"
+  "CMakeFiles/Better_Calculator.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Better_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Better_Calculator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Better_Calculator.
+# This may be replaced when dependencies are built.
